35c0f085a380f7361f7ab243b14b6f5bab43bfaa
@sandyGonzalez <sandygonzalez.612@gmail.com>
2019-06-11 17:59:18 -0500

    subiendo repo en labo

---
+++ src/index.js
@@ -0,0 +1,16 @@
+
+const getNip = document.getElementById('get-nip').value
+const getText = document.getElementById('get-text').value
+const getNumber = document.getElementById('get-number').value
+
+//Cambiar funciones a funciones tipo flecha.
+
+function upperCase(){
+let MayusText= document.getElementById("get-text").value;
+document.getElementById("get-text").value= MayusText.toUpperCase();
+}
+
+function reemplazarAcentos (){
+    
+
+}
